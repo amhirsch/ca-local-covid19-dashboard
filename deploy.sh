ed466e509cb41b201614f427de3877dcf2a4362a
Xander Hirsch <x.hirsch@icloud.com>
2021-10-09 17:11:04 -0700

    Prints error message upon failed deploy

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ NAME='ca-local-covid-19-dashboard'
 docker buildx build --platform linux/amd64 --tag $NAME . \
     && docker tag $NAME "registry.heroku.com/$NAME/web" \
     && docker push "registry.heroku.com/$NAME/web" \
-    && heroku container:release web --app $NAME
+    && heroku container:release web --app $NAME \
+    || echo "-------- Something went wrong! --------"
 docker image prune
